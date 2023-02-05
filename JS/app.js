05b24a3482f975c42956b715937e374c468d8f25
Theodore-ui <theodore.lorenc@netmatters-scs.com>
2023-02-05 15:02:58 +0000

    I have added the html and started styling the side menu

--- JS/app.js
+++ JS/app.js
@@ -1,2 +1,17 @@
-// Banner Slider
 
+alert('This website uses cookies');
+
+
+
+//Header JS
+
+$('.header').sticky(); //sticky Header
+
+
+search_icon_2.addEventListener('click', function() {
+    if (search_2.classList.contains('active')) {
+        search_2.classList.remove('active');
+    } else {
+        search_2.classList.add('active');  
+    }   
+});
