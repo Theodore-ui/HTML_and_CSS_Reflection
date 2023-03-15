578aa528e18879805d64a996c84a6e7dadde5115
Theodore-ui <theodore.lorenc@netmatters-scs.com>
2023-03-15 15:20:37 +0000

    I have converted the header and footer into reusable  files

---
+++ header.php
@@ -0,0 +1,11 @@
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width", initial-scale="1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="JS/slick/slick.css">
+    <link rel="stylesheet" href="JS/slick/slick-theme.css">
+    <link rel="stylesheet" href="css/main.css">
+    <title>Netmatters Home Page</title>
+</head>
--- index.php
+++ index.php
@@ -1,15 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width", initial-scale="1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <link rel="stylesheet" href="css/normalize.css">
-        <link rel="stylesheet" href="JS/slick/slick.css">
-        <link rel="stylesheet" href="JS/slick/slick-theme.css">
-        <link rel="stylesheet" href="css/main.css">
-        <title>Netmatters Home Page</title>
-    </head>
+    <?php include('header.php')?>
     <body>
         <div class="cover"></div>
         <div class="main_column">
@@ -686,86 +677,7 @@
                     </form>
                 </div>    
             </main>
-            <footer>
-                <div class="footer_wrapper">
-                    <div class="footer_card_container">
-                        <div class="services_about">
-                            <div class="footer_card about">
-                                <h4>ABOUT NETMATTERS</h5>
-                                <ul>
-                                    <li><a href="#" target="_self">News</a></li>
-                                    <li><a href="#" target="_self">Our Careers</a></li>
-                                    <li><a href="#" target="_self">Our Team</a></li>
-                                    <li><a href="#" target="_self">Privacy Policy</a></li>
-                                    <li><a href="#" target="_self">Cookie Policy</a></li>
-                                    <li><a href="#" target="_self">Terms & Conditions</a></li>
-                                    <li><a href="#" target="_self">Environmental Policy</a></li>
-                                    <li><a href="#" target="_self">Contact Us</a></li>
-                                </ul>
-                            </div>
-                            <div class="footer_card services">
-                                <h4>OUR SERVICES</h4>
-                                <ul>
-                                    <li><a href="#" target="_self">Bespoke Software</a></li>
-                                    <li><a href="#" target="_self">IT Support</a></li>
-                                    <li><a href="#" target="_self">Digital Marketing</a></li>
-                                    <li><a href="#" target="_self">Telecoms Services</a></li>
-                                    <li><a href="#" target="_self">Web Design</a></li>
-                                    <li><a href="#" target="_self">Cyber Security</a></li>
-                                    <li><a href="#" target="_self">Developer Training</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="offices">
-                            <div class="footer_card cam_off">
-                                <h4>CAMBRIDGE OFFICE</h4>
-                                <ul>
-                                    <li>Unit 1.31,</li>
-                                    <li>St John's Innovation Centre,</li>
-                                    <li>Cowley Road, Miltion
-                                    <li>Cambridge,</li>
-                                    <li>CB4 0WS</li><br>
-                                    <li>Tel: <a href="#" target="_self">01223 37 57 72</a></li>
-                                </ul>
-                            </div>
-                            <div class="footer_card wym_off">
-                                <h4>WYMONDHAM OFFICE</h4>
-                                <ul>
-                                    <li>Unit 15,</li>
-                                    <li>Penfold Drive,</li>
-                                    <li>Gateway 11 Business Park,</li>
-                                    <li>Wymondham, Norfolk,</li>
-                                    <li>NR18 0WZ</li><br>
-                                    <li>Tel: <a href="#" target="_self">01603 70 40 20</a></li>
-                                </ul>
-                            </div>
-                            <div class="footer_card yar_off">
-                                <h4>GREAT YARMOUTH OFFICE</h4>
-                                <ul>
-                                    <li>Suite F23,</li>
-                                    <li>Beacon Innovation Centre,</li>
-                                    <li>Beacon Park, Gorleston,</li>
-                                    <li>Great Yarmouth, Norfolk,</li>
-                                    <li>NR31 7RA</li><br>
-                                    <li>Tel: <a href="#" target="_self">01493 60 32 04</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="bottom">
-                        <div class="copyright">
-                            <span>&copy; Copyright Netmatters 2023.<br class="lb">&nbsp;All rights reserved.</span>   
-                            <span>&nbsp;-&nbsp;<a rhef="#" target="self">sitemap</a></span>
-                        </div>
-                        <div class="social_media">
-                            <a href="#" target="_blank"><i class="fa-brands fa-facebook-f icon icon_f"></i></a>
-                            <a href="#" target="_blank"><i class="fa-brands fa-twitter icon icon_t"></i></a>
-                            <a href="#" target="_blank"><i class="fa-brands fa-instagram icon icon_i"></i></a>
-                            <a href="#" target="_blank"><i class="fa-brands fa-linkedin-in icon icon_l"></i></a>
-                        </div>        
-                    </div> 
-                </div>                                
-            </footer>
+            <?php include('footer.php') ?>
         </div>
         <div class="sidemenu">
             <div class="sidemenu_services_expanded">
